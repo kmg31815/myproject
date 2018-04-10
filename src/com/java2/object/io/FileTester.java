95886fd6b704a2a08a7e8349d8f526dac10029b4
kmg <kmg31815@gmail.com>
2018-04-10 16:27:40 +0800

    FileTester

---
+++ src/com/java2/object/io/FileTester.java
@@ -0,0 +1,21 @@
+package com.java2.object.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
