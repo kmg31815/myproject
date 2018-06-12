12d25215f81c0c2ea523b56a6f862072af983221
kmg <kmg31815@gmail.com>
2018-06-12 16:30:34 +0800

    Bingo

--- src/com/java2/oo/MazeMain.java
+++ src/com/java2/oo/MazeMain.java
@@ -1,9 +1,43 @@
 package com.java2.oo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MazeMain {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		int n = 1;
+		int hp = 100;
+		Random r = new Random();
+		int k[][] = new int[r.nextInt(5)][r.nextInt(5)];
+
+		while (hp != 0) {
+			System.out.print("請輸入指令:");
+			String line = scanner.nextLine();
+			n = Integer.parseInt(line);
+			switch (n) {
+			case 2:
+
+				hp = hp - 1;
+				break;
+			case 4:
+
+				hp = hp - 1;
+				break;
+			case 6:
+
+				hp = hp - 1;
+				break;
+			case 8:
+
+				hp = hp - 1;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 }
